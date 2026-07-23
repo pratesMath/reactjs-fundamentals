66e5e8af5f21467f0583131e6ba4233bb7f1bcca
pratesMath <developer.prates.matheus@gmail.com>
2026-07-23 16:27:07 -0300

    feat: Intro to ReactJS props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Post } from './Post';
+
 export function App() {
 	return (
 		<>
 			<h1>Hello World</h1>
+			<Post
+				author="John Doe"
+				content="This is a sample post."
+			/>
 		</>
 	);
 }
