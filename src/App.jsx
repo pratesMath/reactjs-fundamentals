17ae83e3e61aeef74947b098bdd6b833f866f9fe
pratesMath <developer.prates.matheus@gmail.com>
2026-07-28 21:53:20 -0300

    feat: Post component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from './components/Header';
-import { Post } from './Post'
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
@@ -14,14 +14,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="John Doe"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/pratesMath.png" />
+          <div className={styles.authorInfo}>
+            <strong>Matheus Prates</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+					title="11 de Maio às 08:13h"
+					dateTime="2022-05-11 08:13:00"
+				>
+					Publicado há 1h
+				</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifólio 🚀</p>
+        <p>👉<a href="#">john.doe/design</a></p>
+        <p><a href="#">#novoprojeto #react #frontend</a></p>
+      </div>
+    </article>
+  )
+}
