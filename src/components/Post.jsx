99fe34e4e5a89018eb335fbd775f4aff10c60d98
pratesMath <developer.prates.matheus@gmail.com>
2026-07-28 22:13:29 -0300

    feat: comment form created and :focus applied for web accessibilty

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,6 +30,18 @@ export function Post() {
           <a href="">#frontend</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
