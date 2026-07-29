49ee9d84933add1869f0cc48e4c71437cfc8c388
pratesMath <developer.prates.matheus@gmail.com>
2026-07-28 22:56:35 -0300

    feat: Avatar component was created and reused in others components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
@@ -5,7 +6,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 export function Comment() {
 	return (
 		<div className={styles.comment}>
-			<img src='https://github.com/pratesMath.png' alt='' />
+			<Avatar hasBorder={false} src="https://github.com/pratesMath.png" alt="" />
 
 			<div className={styles.commentBox}>
 				<div className={styles.commentContent}>
