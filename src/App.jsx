acf3d978afa6c3869313d5695bd6974b3e473370
pratesMath <developer.prates.matheus@gmail.com>
2026-08-04 22:48:44 -0300

    chore: jsx iterator w/ map() added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
 import { Header } from './components/Header';
-import { Post } from './components/Post';
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
 import './global.css';
 
-export function App() {
-	return (
-		<div>
-			<Header />
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/pratesMath.png',
+      name: 'Matheus Prates',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'New project on my github 🚀' },
+      { type: 'link', content: 'john.doe/design' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/pratesMath.png',
+      name: 'John Doe',
+      role: 'Unknown'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'New project on my github 🚀' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 
-			<div className={styles.wrapper}>
-				<Sidebar />
-				<main>
-					<Post />
-					<Post />
-				</main>
-			</div>
-		</div>
-	);
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
 }
