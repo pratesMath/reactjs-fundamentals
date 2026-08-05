98ae8ed165dee9c069239d732e6cee2287f02644
pratesMath <developer.prates.matheus@gmail.com>
2026-08-04 23:05:04 -0300

    feat: React props added to Post component and date-fns lib added to INTL format

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,51 +1,64 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post(props) {
-	console.log(props)
-	return (
-		<article className={styles.post}>
-			<header>
-				<div className={styles.author}>
-					<Avatar hasBorder={false} src="https://github.com/pratesMath.png" alt="" />
-					<div className={styles.authorInfo}>
-						<strong>Matheus Prates</strong>
-						<span>Web Developer</span>
-					</div>
-				</div>
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
 
-				<time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:00'>
-					Publicado há 1h
-				</time>
-			</header>
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
 
-			<div className={styles.content}>
-				<p>Fala galera 👋</p>
-				<p>Acabei de subir mais um projeto no meu portifólio 🚀</p>
-				<p>
-					<a href=''>john.doe/design</a>
-				</p>
-				<p>
-					<a href=''>#novoprojeto</a> <a href=''>#react</a> <a href=''>#frontend</a>
-				</p>
-			</div>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-			<form className={styles.commentForm}>
-				<strong>Deixe seu feedback</strong>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-				<textarea placeholder='Deixe um comentário' />
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
 
-				<footer>
-					<button type='submit'>Publicar</button>
-				</footer>
-			</form>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
-			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
-				<Comment />
-			</div>
-		</article>
-	);
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  )
 }
